ad9dfab99c50a761ecf2bc779f184effe391d4b6
sangbaek <aumsbk@naver.com>
2022-04-04 20:05:41 +0900

    impact museum 2.93(브랜드 상세페이지 업데이트)

--- posts/views.py
+++ posts/views.py
@@ -16,8 +16,18 @@ class BrandListView(ListView):              # Brand 모델 데이터 ListVIew로
 
 
 
+# 브랜드 상세페이지 보여주기
 def brand(request, brand_id):
 
-    return render(request, 'posts/brand.html')
+    brand = Brand.objects.get(id=brand_id)   # brand_id로 brand 데이터 1개 조회
+
+    products = brand.brand_product.all()     # 해당 brand에 속한 products 데이터만 조회하기
+
+    context = {
+        'brand': brand,
+        'products': products,
+    }
+
+    return render(request, 'posts/brand.html', context)
 
 
